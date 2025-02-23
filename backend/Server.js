a9f33dec4af0634d44f464782b09255457d41c11
Pasindu Hansana <gallagepasinduhansana@gmail.com>
2025-02-23 16:33:51 +0530

    initial touch

--- backend/Server.js
+++ backend/Server.js
@@ -6,29 +6,19 @@ import AuthRoutes from "./Routes/Auth.js";
 import AdminRoutes from "./Routes/AdminRoutes.js";
 import cookieparser from "cookie-parser";
 import cloudinary from "cloudinary";
-import productsRoute from "./Routes/ProductRoute.js";
-import cart from "./Routes/CartRoute.js";
-import purchaseHistoryRoute from "./Routes/PurchaseHistoryRoute.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-cloudinary.config({
-  cloud_name: process.env.cloud_name,
-  api_key: process.env.api_key,
-  api_secret: process.env.api_secret,
-});
-
 Dbcon();
+
 app.use(express.json());
 const allowedOrigins = [
-  // "http://localhost:5173",
-  // "http://localhost:3001",
   "https://artisanbespokefurniture.com",
   "https://www.artisanbespokefurniture.com/",
-  "https://www.artisanbespokefurniture.com"
+  "https://www.artisanbespokefurniture.com",
 ];
 
 app.use(
@@ -48,9 +38,6 @@ app.use(cookieparser());
 
 app.use("/api/auth", AuthRoutes);
 app.use("/api/admin", AdminRoutes);
-app.use("/api/v1/products", productsRoute);
-app.use("/api/v1/cart", cart);
-app.use("/api/v1/purchase-history", purchaseHistoryRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
