b6a81c91d7f47da990b99478cabf93349ec5292f
Suhansa kashmira <it22059772@my.sliit.lk>
2025-03-19 15:26:43 +0530

    Refactor app.js and App.jsx to organize middleware and component imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,117 @@ import axios from 'axios';
 import PostView from './Pages/PostView';
 import AdminPanel from './Pages/AdminPanel';
 
+//Main Components Imports 
+
+
+
+
+
+
+
+//Disaster management imports
+
+
+
+
+
+
+
+
+
+
+//Comunity-support imports
+
+
+
+
+
+
+
+
+
+//Disaster funding imports 
+
+
+
+
+
+
+
+
+
+
+//Admin dashbord imports 
+
+
+
+
+
+
+
+
+
+
+
 function App() {
   return (
+
+
+
+
+
+    
     <Router>
       <Routes>
+        {/* Main Components */}
+
+
+
+
+
+
+        {/* Disaster management */}
+
+
+
+
+
+
+
+
+
+        {/* Comunity-support */}
         <Route path="/adminposts" element={<AdminPanel />} />
         <Route path="/posts" element={<PostView />} />
+
+
+
+
+
+
+
+
+        {/* Disaster funding */}
+
+
+
+
+
+
+
+
+
+
+        {/* Admin dashbord */}
+
+
+
+
+
+
+
+
+      
       </Routes>
     </Router>
   );
